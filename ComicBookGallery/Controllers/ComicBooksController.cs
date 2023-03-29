5983fdf689715faa39d4e8818c9f244ca5729a1b
Sophia Spieler <Sophia.Spieler@de.rhenus.com>
2023-03-29 07:42:25 +0200

    feat: Update Detail action method to use action result type

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,16 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic bookc controller!";
+            //Redirect Result = Action Content Type
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday) {
+
+                return Redirect("/");
+            }
+
+            //Content Result = Action Content Type
+            return Content("Hello from the Comic Book Controller!");
         }
     }
 }
