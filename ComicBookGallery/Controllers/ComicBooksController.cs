b869ddfdfb89f78d2bfab3f20110e95fe0988538
Sophia Spieler <Sophia.Spieler@de.rhenus.com>
2023-03-29 09:53:39 +0200

    feat: Update comic book detail view

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -19,6 +19,19 @@ namespace ComicBookGallery.Controllers
             //Content Result = Action Content Type
             return Content("Hello from the Comic Book Controller!");*/
 
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+
             return View();
         }
     }
